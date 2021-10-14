42436d25ff8ababfc959bcb52240a890cb4fb1ac
Drew Kimberly <drew.kimberly@citrix.com>
2021-10-14 12:16:28 -0400

    feat: associate user berito to VM Users group

--- terraform/aad-user-groups.tf
+++ terraform/aad-user-groups.tf
@@ -1,5 +1,3 @@
-data "azuread_client_config" "current" {}
-
 resource "azuread_group" "vm_users" {
   display_name     = "VM Users"
   owners           = [data.azuread_client_config.current.object_id]
@@ -10,6 +8,11 @@ resource "random_password" "berito_pw" {
   length = 16
 }
 
+output "berito_pw" {
+  value     = random_password.berito_pw.result
+  sensitive = true
+}
+
 resource "azuread_user" "berito" {
   user_principal_name = format("berito@%s", var.aad_domain_name)
   password            = random_password.berito_pw.result
@@ -18,7 +21,7 @@ resource "azuread_user" "berito" {
   surname             = "Berit"
 }
 
-output "berito_pw" {
-  value     = random_password.berito_pw.result
-  sensitive = true
+resource "azuread_group_member" "berito_vm_users" {
+  group_object_id  = azuread_group.vm_users.object_id
+  member_object_id = azuread_user.berito.object_id
 }
